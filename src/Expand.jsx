8d20bf02bb9e2ce9a2c61955df0b6834a7624989
Viktoria <doduchenko29@gmail.com>
2021-07-19 19:26:09 +0300

    add

--- src/Expand.jsx
+++ src/Expand.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 
-class Expand extends React.Component {
-  state = {
-    hide: false,
-  };
-
-  render() {
-    const { hide } = this.state;
-    return (
-      <div className="app">
-        <div className="expand border">
-          <div className="expand__header">
-            <span className="expand__title">{this.props.title}</span>
-            <button
-              onClick={() => this.setState({ hide: !hide })}
-              className="expand__toggle-btn"
-            >
-              {this.state.hide ? (
-                <i className="fas fa-chevron-up"></i>
-              ) : (
-                <i className="fas fa-chevron-down"></i>
-              )}
-            </button>
-          </div>
-          <div className="expand__content">
-            {this.state.hide ? this.props.elem : null}
-          </div>
-        </div>
+const Expand = ({ isOpen, children, title, showText }) => {
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button onClick={showText} className="expand__toggle-btn">
+          {isOpen ? (
+            <i className="fas fa-chevron-up"></i>
+          ) : (
+            <i className="fas fa-chevron-down"></i>
+          )}
+        </button>
       </div>
-    );
-  }
-}
+      {isOpen ? <div className="expand__content">{children}</div> : null}
+      
+    </div>
+  );
+};
 
 export default Expand;
+
+// class Expand extends React.Component {
+//   state = {
+//     hide: false,
+//   };
+
+//   render() {
+//     const { hide } = this.state;
+//     return (
+//       <div className="app">
+//         <div className="expand border">
+//           <div className="expand__header">
+//             <span className="expand__title">{this.props.title}</span>
+//             <button
+//               onClick={() => this.setState({ hide: !hide })}
+//               className="expand__toggle-btn"
+//             >
+//               {this.state.hide ? (
+//                 <i className="fas fa-chevron-up"></i>
+//               ) : (
+//                 <i className="fas fa-chevron-down"></i>
+//               )}
+//             </button>
+//           </div>
+//           <div className="expand__content">
+//             {this.state.hide ? this.props.elem : null}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import Expand from './Expand';
+import App from './App';
 
 const rootElement = document.querySelector("#root");
 
-const elem = (
-  <p>
-    Hooks are a new addition in React 16.8. They let you use state and other
-    React features without writing a class.
-  </p>
-);
+ReactDOM.render(<App />, rootElement);
 
-ReactDOM.render(<Expand title="Some title" elem={elem}/>, rootElement);
+// const elem = (
+//   <p>
+//     Hooks are a new addition in React 16.8. They let you use state and other
+//     React features without writing a class.
+//   </p>
+// );
+
+// ReactDOM.render(<Expand title="Some title" elem={elem}/>, rootElement);
